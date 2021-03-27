b12063b562e87f896f852d34cd369992ead6ad6f
ybj1997 <973122310@qq.com>
2021-03-27 15:29:36 +0800

    JS高级学习

--- JS/JS高级/11自定义JS模块/自调用版本.js
+++ JS/JS高级/11自定义JS模块/自调用版本.js
@@ -14,3 +14,19 @@
         downcase:downcase
     }
 })()
+//优化方案
+//(function(anyvalue){
+//    var a = 'abc';
+//    //创建模块方法
+//    function uppercase (){
+//        console.log('这个模块是大写的' + a.toUpperCase());
+//    }
+//    function downcase (){
+//        console.log('这个模块是小写的' + a.toLowerCase());
+//    }
+//    //向外暴露的调用接口
+//    anyvalue.NewMoudle2 = {
+//        uppercase:uppercase,
+//        downcase:downcase
+//    }
+//})(window)
