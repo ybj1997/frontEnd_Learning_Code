323e619443e7b3c15ecd1d2201408e7ccabbada3
ybj1997 <973122310@qq.com>
2021-04-07 14:25:28 +0800

    jQuery方法AJAX

--- Ajax/jQuery-AJAX/server.js
+++ Ajax/jQuery-AJAX/server.js
@@ -5,13 +5,11 @@ const express = require('express');
 const app = express();
 
 //3.创建路由规则
-app.get('/server',(request,response)=>{
+app.all('/jQuery',(request,response)=>{
     //设置相应头 设置允许跨域
     response.setHeader('Access-Control-Allow-Origin','*');
-    //设置相应头
-    response.setHeader('Access-Control-Allow-Headers','*')
     //设置响应体
-    response.send('String');
+    response.send('Hello jQuery AJAX');
 });
 
 //4.监听端口启动服务
