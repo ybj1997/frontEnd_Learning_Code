baf415446f02dd2135125005e7c5e6ee96b19a33
Allenyan <973122310@qq.com>
2021-11-14 20:53:03 +0800

    2021/10/14/webpack自学/开发环境配置总结

---
+++ webpack/开发环境配置/webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {resolve} = require('path');
+module.exports = {
+    entry:'./src/index.js',
+    output:{
+        filename:'build.js',
+        path:resolve(__dirname, 'build')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test:/\.less$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test:/\.(png|jpg|gif)$/,
+                loader:'url-loader',
+                options:{
+                    limit: 8*1024,
+                    name:'[hash:5].[ext]',
+                    esModule:false
+                }
+            },
+            {
+                test:/\.html$/,
+                loader:'html-withimg-loader'
+            },
+            {
+                exclude:/\.(html|css|less|png|jpg|gif)/,
+                loader:'file-loader'
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    mode: 'development',
+    devServer: {
+        contentBase:resolve(__dirname,'build'),//编译文件夹
+        compress:true,
+        open: true,
+        port:3000
+    }
+}
